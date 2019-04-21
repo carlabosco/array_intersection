a90c1de5f3c24a3fc9f748cc46768b164bbd39ed
Carla Bosco <carlabosco@gmail.com>
2019-04-21 11:43:04 -0700

    Added method with time and space complexities

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,19 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(m*n), where m is size of first array and n is the size of second array
+# Space complexity: O(m), where m is the size of the smallest array
 def intersection(array1, array2)
-  raise NotImplementedError
+  result = []
+
+  if array1 == nil || array2 == nil
+    result = []
+  else
+    array1.each do |n|
+      array2.each do |m|
+        if n == m
+          result << n
+        end
+      end
+    end
+  end
+  return result
 end
